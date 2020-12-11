34d5cc7f71046e971aa7cc583b2a5341ad49fd1f
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-11 12:54:27 -0800

    Made register page with blank form & routes to it. Created templates/register.html and app.py.

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "kubrick"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
+app.config["SLQALCHEMY_TRACK_MODIFICATIONS"] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route("/")
+def go_to_register_page():
+    """
+        Redirects to register page
+        rtype: str
+    """
+    return redirect("/register")
+
+@app.route("/register")
+def show_register_page():
+    """
+        Shows form for user to register
+        rtype: str
+    """
+    return render_template("register.html")
